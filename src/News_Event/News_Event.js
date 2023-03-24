0d6f8db7ba97dadde4ba36c4dbeb13b0f9f65ce1
youngmentor <olaleresuliton43@gmail.com>
2023-03-24 21:18:40 +0100

    new commit

--- src/News_Event/News_Event.js
+++ src/News_Event/News_Event.js
@@ -5,12 +5,13 @@ import Home from './images/HomeMonitoring.jpg'
 import "./News_Event.css"
 import ContactCard from '../COMPONENT/CONTACT_CARD/Contact_card'
 import Four from "./images/Four.jpg"
+
 const News_Event = () => {
     return (
         <div className='News_Event_Main'>
             <Image image={Home} text="News and event" />
             <div className='News_Event_Card_Wrap'>
-              <ContactCard  image={Four} arr={[1, 2, 3, 4, 5,6,7,8,9,10,11,12]} />    
+              <ContactCard  image={Four} arr={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]} />    
             </div>
         </div>
     )
